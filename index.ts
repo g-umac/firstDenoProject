ece828f9aa0a19fe9d1bdcd2b9c75345268689c2
g-umac <gamzeumac49@gmail.com>
2022-11-19 20:25:50 +0100

    settings json for vscode completed

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
-import { bgBlue, red, bold,   green, underline, blue, } from "https://deno.land/std@0.165.0/fmt/colors.ts";
+import {
+  bgBlue,
+  blue,
+  bold,
+  green,
+  red,
+  underline,
+} from "https://deno.land/std@0.165.0/fmt/colors.ts";
 
-let myName: string; 
+let myName: string;
 
 myName = "Gamze";
- 
- 
-  console.log(`My name is ${green(myName)}`);
-  console.log(`My name is ${blue(myName)}`);
-  console.log(`My name is ${bold(myName)}`);
-  console.log(`My name is ${underline(green(myName))}`);
+
+console.log(`My name is ${green(myName)}`);
+console.log(`My name is ${blue(myName)}`);
+console.log(`My name is ${bold(myName)}`);
+console.log(`My name is ${underline(green(myName))}`);
